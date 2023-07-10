f10cb4fe3885bae0af6aee4a2fd917faccf88c17
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-10 22:46:07 +0000

    app updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Routes>
+      <Route path="/" exact element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 

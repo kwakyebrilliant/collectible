b78ec1b4a1629aff7603d0069f171ad68ef10d3a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-12 22:19:09 +0000

    profile updates

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from 'react';
 import Navigation from '../components/Navigation'
@@ -35,6 +36,15 @@ function Profile() {
   };
 
   const tabNames = ['Your Uploads', 'Acquired'];
+
+  const [showModal, setShowModal] = useState(false);
+
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
 
   return (
     <div className='bg-black'>
@@ -83,7 +93,7 @@ function Profile() {
                 <div className="flex items-center justify-end"> {/* Add this div and class */}
                     <a
                         className="flex text-center cursor-pointer items-center px-8 py-2 mx-2 hover:text-black text-white bg-transparent border border-white rounded hover:bg-white active:text-white focus:outline-none focus:ring"
-                        type="submit"
+                        onClick={() => setShowModal(true)}
                     >
                         <FaPlusCircle className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
                         <span className="text-sm font-medium">
@@ -93,6 +103,154 @@ function Profile() {
                 </div>
             </div>
 
+            {showModal ? (
+                                <>
+                                <div className="justify-center items-center flex overflow-x-scroll overflow-y-scroll top-10 fixed pt-[20%] inset-0 z-50 outline-none focus:outline-none">
+                                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                                    {/*content*/}
+                                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-100 outline-none focus:outline-none">
+                                        {/*header*/}
+                                        <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                        <h3 className="font-medium text-lg text-gray-900">
+                                            Add Collectibles
+                                        </h3>
+                                        <button
+                                            className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                            onClick={() => setShowModal(false)}
+                                        >
+                                            <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                            ×
+                                            </span>
+                                        </button>
+                                        </div>
+                                        {/*body*/}
+                                        <div className="relative lg:w-[700px] w-80 space-y-4 p-6 flex-auto">
+                            
+
+                                        <div className='pt-4'>
+
+                                          <div className="relative">
+                                          <input
+                                              type="text"
+                                              className="w-full bg-gray-200 focus:outline-none rounded-lg border-gray-200 p-4 pe-12 text-sm"
+                                              placeholder="Enter Title"
+                                          />
+                                          </div>
+                                        </div>
+
+                                        <div className='pt-4'>
+
+                                          <div className="relative">
+                                          <input
+                                              type="number"
+                                              className="w-full bg-gray-200 focus:outline-none rounded-lg border-gray-200 p-4 pe-12 text-sm"
+                                              placeholder="Enter Price"
+                                          />
+                                          </div>
+                                          </div>
+
+                                          <div className='pt-4'>
+
+                                            <div className="relative">
+                                            <select className="w-full bg-gray-200 focus:outline-none rounded-md bg-transparent border-none p-4 pe-12 text-sm sm:text-sm">
+                                            <option value="">Select Category</option>
+                                            <option value="image">Image</option>
+                                            <option value="video">Video</option>
+                                            </select>
+                                            </div>
+                                        </div>
+
+                                        <div className='pt-4'>
+
+                                          <div className="relative">
+                                          <textarea
+                                              type="text"
+                                              className="w-full bg-gray-50 focus:outline-none rounded-lg border-gray-200 p-4 pe-12 text-sm"
+                                              placeholder="Describe collectible"
+                                              rows={4}
+                                          />
+                                          </div>
+                                      </div>
+
+                                      <div className="flex flex-col items-center justify-center bg-gray-100">
+                                        <div className="group w-full relative bg-gray-100 p-2 py-4 shadow-lg rounded-lg">
+
+                                        <label
+                                          htmlFor="image-file-input"
+                                          className="relative items-center justify-center w-full h-12 px-4 py-2 text-sm font-medium text-black bg-white rounded-md cursor-pointer hover:bg-black hover:text-white focus:outline-none"
+                                        >
+                                          Choose File
+                                          <input
+                                            id="image-file-input"
+                                            type="file"
+                                            accept="image/*, video/*"
+                                            onChange={handleFileChange}
+                                            className="hidden"
+                                          />
+                                        </label>
+
+                                          {selectedFile && (
+                                            <div className='m-2'>
+                                              {selectedFile.type.startsWith('image/') ? (
+                                                <img
+                                                  src={URL.createObjectURL(selectedFile)}
+                                                  alt="Uploaded Image"
+                                                  className="object-cover w-full h-60 mt-4 rounded-lg"
+                                                />
+                                              ) : (
+                                                <video
+                                                  src={URL.createObjectURL(selectedFile)}
+                                                  controls
+                                                  className="w-full h-60 mt-4 rounded-lg"
+                                                />
+                                              )}
+                                            </div>
+                                          )}
+                                        </div>
+                                      </div>
+
+
+
+                                    <a className="group w-full relative inline-flex items-center overflow-hidden rounded bg-green-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-green-600" href="/">
+                                        <span className="absolute left-0 -translate-x-full transition-transform group-hover:translate-x-4">
+                                            <svg
+                                            className="h-5 w-5"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                            >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M17 8l4 4m0 0l-4 4m4-4H3"
+                                            />
+                                            </svg>
+                                        </span>
+
+                                        <span className="text-sm font-medium transition-all group-hover:ml-4">
+                                            Submit
+                                        </span>
+                                        </a>
+
+                                        </div>
+                                        {/*footer*/}
+                                        <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                        <a
+                                            className="text-white bg-black cursor-pointer rounded-lg hover:bg-red-950 hover:text-white background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                            type="button"
+                                            onClick={() => setShowModal(false)}
+                                        >
+                                            Close
+                                        </a>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                                </>
+                            ) : null}
 
 
 

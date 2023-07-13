fc316b574b34db41b0634e1ab75f64e515f651a4
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-13 20:07:23 +0000

    shop updates

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -135,102 +135,127 @@ function Shop() {
                 </div>
                 </div>
 
-                <ul class="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+                <ul className="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
                 <li>
-                    <a href="#" class="group block overflow-hidden">
+                <a href="#" className="group relative bg-black">
                     <img
-                        src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                        alt=""
-                        class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                    alt="Developer"
+                    src={Try}
+                    className="absolute rounded-lg inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 pb-8"
                     />
 
-                    <div class="relative bg-white pt-3">
-                        <h3
-                        class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+                    <div className="relative p-4 sm:p-6 lg:p-8">
+                    <p className="text-sm font-medium uppercase tracking-widest text-emerald-600">
+                        Developer
+                    </p>
+
+                    <p className="text-xl font-bold text-white sm:text-2xl">Tony Wayne</p>
+
+                    <div className="mt-32 sm:mt-48 lg:mt-64">
+                        <div
+                        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
                         >
-                        Basic Tee
-                        </h3>
-
-                        <p class="mt-2">
-                        <span class="sr-only"> Regular Price </span>
-
-                        <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
+                        <p className="text-sm text-white">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+                            perferendis hic asperiores quibusdam quidem voluptates doloremque
+                            reiciendis nostrum harum. Repudiandae?
                         </p>
+                        </div>
                     </div>
-                    </a>
+                    </div>
+                </a>
                 </li>
 
                 <li>
-                    <a href="#" class="group block overflow-hidden">
+                <a href="#" className="group relative bg-black">
                     <img
-                        src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                        alt=""
-                        class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                    alt="Developer"
+                    src={Try}
+                    className="absolute rounded-lg inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 pb-8"
                     />
 
-                    <div class="relative bg-white pt-3">
-                        <h3
-                        class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+                    <div className="relative p-4 sm:p-6 lg:p-8">
+                    <p className="text-sm font-medium uppercase tracking-widest text-emerald-600">
+                        Developer
+                    </p>
+
+                    <p className="text-xl font-bold text-white sm:text-2xl">Tony Wayne</p>
+
+                    <div className="mt-32 sm:mt-48 lg:mt-64">
+                        <div
+                        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
                         >
-                        Basic Tee
-                        </h3>
-
-                        <p class="mt-2">
-                        <span class="sr-only"> Regular Price </span>
-
-                        <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
+                        <p className="text-sm text-white">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+                            perferendis hic asperiores quibusdam quidem voluptates doloremque
+                            reiciendis nostrum harum. Repudiandae?
                         </p>
+                        </div>
                     </div>
-                    </a>
+                    </div>
+                </a>
                 </li>
 
                 <li>
-                    <a href="#" class="group block overflow-hidden">
+                <a href="#" className="group relative bg-black">
                     <img
-                        src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                        alt=""
-                        class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                    alt="Developer"
+                    src={Try}
+                    className="absolute rounded-lg inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 pb-8"
                     />
 
-                    <div class="relative bg-white pt-3">
-                        <h3
-                        class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+                    <div className="relative p-4 sm:p-6 lg:p-8">
+                    <p className="text-sm font-medium uppercase tracking-widest text-emerald-600">
+                        Developer
+                    </p>
+
+                    <p className="text-xl font-bold text-white sm:text-2xl">Tony Wayne</p>
+
+                    <div className="mt-32 sm:mt-48 lg:mt-64">
+                        <div
+                        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
                         >
-                        Basic Tee
-                        </h3>
-
-                        <p class="mt-2">
-                        <span class="sr-only"> Regular Price </span>
-
-                        <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
+                        <p className="text-sm text-white">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+                            perferendis hic asperiores quibusdam quidem voluptates doloremque
+                            reiciendis nostrum harum. Repudiandae?
                         </p>
+                        </div>
                     </div>
-                    </a>
+                    </div>
+                </a>
                 </li>
 
                 <li>
-                    <a href="#" class="group block overflow-hidden">
+                <a href="#" className="group relative bg-black">
                     <img
-                        src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                        alt=""
-                        class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                    alt="Developer"
+                    src={Try}
+                    className="absolute rounded-lg inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 pb-8"
                     />
 
-                    <div class="relative bg-white pt-3">
-                        <h3
-                        class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+                    <div className="relative p-4 sm:p-6 lg:p-8">
+                    <p className="text-sm font-medium uppercase tracking-widest text-emerald-600">
+                        Developer
+                    </p>
+
+                    <p className="text-xl font-bold text-white sm:text-2xl">Tony Wayne</p>
+
+                    <div className="mt-32 sm:mt-48 lg:mt-64">
+                        <div
+                        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
                         >
-                        Basic Tee
-                        </h3>
-
-                        <p class="mt-2">
-                        <span class="sr-only"> Regular Price </span>
-
-                        <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
+                        <p className="text-sm text-white">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+                            perferendis hic asperiores quibusdam quidem voluptates doloremque
+                            reiciendis nostrum harum. Repudiandae?
                         </p>
+                        </div>
                     </div>
-                    </a>
+                    </div>
+                </a>
                 </li>
+                
                 </ul>
             </div>
             </section>

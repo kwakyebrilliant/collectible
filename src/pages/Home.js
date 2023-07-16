b9686681f6f2314fa80d467cd6511e6e3ee6df7b
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-16 12:48:48 +0000

    home updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -67,7 +67,7 @@ function Home() {
         <Navigation />
         
 
-        <div className="pt-32 lg:flex mx-auto justify-center">
+        <div className="lg:pt-32 pt-8 lg:flex mx-auto justify-center">
           <section>
             <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
               <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
@@ -87,7 +87,7 @@ function Home() {
                     <li>
                       <a href="/single" className="bg-gray-100 flex rounded">
                         <img
-                          src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80"
+                          src={Try}
                           alt=""
                           className="object-cover w-full rounded aspect-square lg:hover:transform lg:hover:-rotate-3 lg:hover:translate-x-2 lg:hover:translate-y-2"
                         />

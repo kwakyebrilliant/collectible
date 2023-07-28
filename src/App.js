e20604c3f01f704b99c4360a7288182f0907fc17
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-28 19:48:46 +0000

    app updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Single from "./pages/Single";
 import Profile from "./pages/Profile";
+import Connect from "./pages/Connect";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/shop" exact element={<Shop />} />
       <Route path="/single" exact element={<Single />} />
       <Route path="/profile" exact element={<Profile />} />
+      <Route path="/connect" exact element={<Connect />} />
       </Routes>
     </Router>
   );

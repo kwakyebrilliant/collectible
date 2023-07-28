d554d1679c7d5d0516e5bebcd1339cc5eefd7b61
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-28 10:53:05 +0000

    collectible updates

--- contracts/Collectible.sol
+++ contracts/Collectible.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.9;
 
 contract Collectible {
+    address public owner;
+    uint256 public totalNFTs;
+
+    struct NFT {
+        address owner;
+        uint256 price;
+        bool status; // true: listed, false: acquired
+        string title;
+        string description;
+        string media;
+    }
+
 
 }

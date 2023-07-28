3668a74080b5c112033989a96a126ba84760b415
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-28 19:54:34 +0000

    connect updates

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { FaArrowCircleLeft } from 'react-icons/fa';
+
+import logo from '../assets/logo.png'
 
 function Connect() {
+
+    const handleGoBack = () => {
+        window.history.back();
+      };
+
   return (
     <div>Connect</div>
   )

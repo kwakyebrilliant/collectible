b0c4753fd3f11eaa45cfeb0a5c239245918c53ec
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-31 12:25:38 +0000

    collectibles updates

--- contracts/Collectible.sol
+++ contracts/Collectible.sol
@@ -19,4 +19,11 @@ contract Collectible {
     mapping(uint256 => NFT) public nfts;
     mapping(address => uint256[]) public userToNFTs;
     mapping(address => uint256[]) public userToAcquiredNFTs;
+
+    //events
+    event NFTCreated(uint256 indexed tokenId, address indexed owner);
+    event NFTListed(uint256 indexed tokenId, uint256 price);
+    event NFTAcquired(uint256 indexed tokenId, address indexed buyer);
+    event NFTResold(uint256 indexed tokenId, uint256 newPrice);
+    
 }
